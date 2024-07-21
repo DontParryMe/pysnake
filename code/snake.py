e147150e11cc93db57cdfe901d0761c958fe745d
DontParryMe <105429071+DontParryMe@users.noreply.github.com>
2024-07-21 16:55:53 +0300

    Update snake.py

--- code/snake.py
+++ code/snake.py
@@ -24,7 +24,6 @@ class Snake:
 
     def import_surfs(self):
         surf_dict = {}
-        # Теперь путь указывает на папку 'graphics', без подпапки 'snake'
         folder_path = resource_path('graphics/snake')
         print(f'Looking in: {folder_path}')
         if not os.path.exists(folder_path):
